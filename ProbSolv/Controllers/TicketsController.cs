7a4ff9d5fb87ca0b44317a7e01b42015bedb5391
Leo Barnuevo <leocoding.net@gmail.com>
2023-02-16 07:18:40 -0600

    ticket archive and restore
    - added ticket archive and restore, both get & post methods, as well as their views

--- ProbSolv/Controllers/TicketsController.cs
+++ ProbSolv/Controllers/TicketsController.cs
@@ -200,21 +200,24 @@ namespace ProbSolv.Controllers
         }
 
         // GET: Tickets/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Archive(int? id)
         {
-            if (id == null || _context.Tickets == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var ticket = await _context.Tickets
-                .Include(t => t.DeveloperUser)
-                .Include(t => t.OwnerUser)
-                .Include(t => t.Project)
-                .Include(t => t.TicketPriority)
-                .Include(t => t.TicketStatus)
-                .Include(t => t.TicketType)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            /* var ticket = await _context.Tickets
+                 .Include(t => t.DeveloperUser)
+                 .Include(t => t.OwnerUser)
+                 .Include(t => t.Project)
+                 .Include(t => t.TicketPriority)
+                 .Include(t => t.TicketStatus)
+                 .Include(t => t.TicketType)
+                 .FirstOrDefaultAsync(m => m.Id == id);*/
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
+
             if (ticket == null)
             {
                 return NotFound();
@@ -224,23 +227,68 @@ namespace ProbSolv.Controllers
         }
 
         // POST: Tickets/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost, ActionName("Archive")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> ArchiveConfirmed(int id)
         {
             if (_context.Tickets == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Tickets'  is null.");
             }
-            var ticket = await _context.Tickets.FindAsync(id);
-            if (ticket != null)
-            {
-                _context.Tickets.Remove(ticket);
-            }
-            
-            await _context.SaveChangesAsync();
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
+            ticket.Archived = true;            
+            await _ticketService.UpdateTicketAsync(ticket);
+
             return RedirectToAction(nameof(Index));
         }
+
+
+        public async Task<IActionResult> Restore(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            /* var ticket = await _context.Tickets
+                 .Include(t => t.DeveloperUser)
+                 .Include(t => t.OwnerUser)
+                 .Include(t => t.Project)
+                 .Include(t => t.TicketPriority)
+                 .Include(t => t.TicketStatus)
+                 .Include(t => t.TicketType)
+                 .FirstOrDefaultAsync(m => m.Id == id);*/
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Restore")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ARestoreConfirmed(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Tickets'  is null.");
+            }
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
+            ticket.Archived = false;
+            await _ticketService.UpdateTicketAsync(ticket);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
 
         private async Task<bool> TicketExists(int id)
         {
