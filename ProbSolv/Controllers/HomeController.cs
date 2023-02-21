203b0cc5ece2c0ede4c2bbe4e3d60c866203467f
Leo Barnuevo <leocoding.net@gmail.com>
2023-02-20 18:03:13 -0600

    Dashboard
    - added dashboard get method to home controller

--- ProbSolv/Controllers/HomeController.cs
+++ ProbSolv/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProbSolv.Extensions;
 using ProbSolv.Models;
+using ProbSolv.Models.ViewModels;
+using ProbSolv.Services.Interfaces;
 using System.Diagnostics;
 
 namespace ProbSolv.Controllers
@@ -7,16 +10,34 @@ namespace ProbSolv.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPSCompanyInfoService _companyinfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPSCompanyInfoService companyinfoService)
         {
             _logger = logger;
+            _companyinfoService = companyinfoService;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyinfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyinfoService.GetAllProjectsAsync(companyId))
+                                                       .Where(p => p.Archived == false)
+                                                       .ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
+        }
+
 
         public IActionResult Privacy()
         {
